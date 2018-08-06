2585aa55d4b42805944e67c5eef04ab2e96fd06c
“rikusv” <rikusv@gmail.com>
2018-08-06 17:57:10 +0200

    Add Firestore setup and update documentation

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyC4wo5etXs1kS4d8fyvxu8IUYTqKZ50n-I',
+    authDomain: 'amena-wedding.firebaseapp.com',
+    databaseURL: 'https://amena-wedding.firebaseio.com',
+    projectId: 'amena-wedding',
+    storageBucket: 'amena-wedding.appspot.com',
+    messagingSenderId: '646979837076'
+  }
 };
