fab6c9a36e7089d119be69362952cf615fabf8e7
“rikusv” <rikusv@gmail.com>
2018-08-07 11:51:08 +0200

    Remove unused constructors

--- src/app/event.ts
+++ src/app/event.ts
@@ -1,16 +1,5 @@
 export class Event {
 
-  constructor(
-    id: string,
-    event: any,
-    max: number,
-    rsvp: number | null
-  ) {
-    this.id = id;
-    this.max = max;
-    Object.keys(event).map(key => this[key] = event[key]);
-  }
-
   id: string;
   name: string;
   time: string;
--- src/app/invitation.ts
+++ src/app/invitation.ts
@@ -2,11 +2,6 @@ import { Event } from './event';
 
 export class Invitation {
 
-  constructor(invitation: any) {
-    this.name = invitation.name;
-    this.events = [];
-  }
-
   name: string;
   phone: number;
   events: Event[];
