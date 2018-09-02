796258f984956e0348e6dd2858c8e51acf7b1f73
“rikusv” <rikusv@gmail.com>
2018-09-02 21:38:32 +0200

    Add sorting on whether all or some people are coming

--- src/app/manage/sort.pipe.ts
+++ src/app/manage/sort.pipe.ts
@@ -7,20 +7,39 @@ export class SortPipe implements PipeTransform {
 
 
   transform(items: any[], sorter: {property: string, ascending?: boolean}): any {
+    function score(invitation: any, eventId: string) {
+      return invitation.rsvp[eventId] === '' || typeof invitation.rsvp[eventId] === 'undefined' ?
+      0 : (invitation.rsvp[eventId] < invitation.events[eventId] ? 2 : 1);
+    }
     if (!items || !sorter || !sorter.property) {
       return items;
     }
-    const type = typeof items[0][sorter.property];
-    switch (type) {
-      case 'boolean':
-        return items.sort((a, b) => a[sorter.property] ?
-        (sorter.ascending ? 1 : -1) : (sorter.ascending ? -1 : 1));
-      case 'string':
-        return items.sort((a, b) => a[sorter.property] < b[sorter.property] ?
-        (sorter.ascending ? -1 : 1) : (sorter.ascending ? 1 : -1));
-      default:
-        return items;
+    const parts = sorter.property.split('/');
+    if (parts.length === 1) { // direct field sort
+      const type = typeof items[0][sorter.property];
+      switch (type) {
+        case 'boolean':
+          return items.sort((a, b) => a[sorter.property] ?
+          (sorter.ascending ? 1 : -1) : (sorter.ascending ? -1 : 1));
+        case 'string':
+          return items.sort((a, b) => a[sorter.property] < b[sorter.property] ?
+          (sorter.ascending ? -1 : 1) : (sorter.ascending ? 1 : -1));
+        default:
+          return items;
+      }
+    } else if (parts[0] === 'rsvp') {
+
+      const eventId = parts[1];
+      return items.sort((a, b) => {
+        const aScore = score(a, eventId);
+        const bScore = score(b, eventId);
+        return sorter.ascending ?
+        (aScore >= bScore ? -1 : 1) : (aScore >= bScore ? 1 : -1);
+      });
+    } else { // don't know how to sort
+      return items;
     }
+
   }
 
 }
